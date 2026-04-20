c3840f60649468d7b53911dd0f2f6ba441579e12
Chinmay kardpati <karodpatichinmay12@gmail.com>
2026-04-20 07:30:50 +0530

    Spring Caching Added

---
+++ caching/src/main/java/com/chinmay/caching/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.chinmay.caching.repositories;
+
+import com.chinmay.caching.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
+}
